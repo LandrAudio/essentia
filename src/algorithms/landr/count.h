08b32a305cb8daf36511e5b22716a26c44587ec5
Guillaume Veck <gveck@landr.com>
2019-04-30 14:10:24 -0400

    Restore memory header inclusion for make_shared and shared_ptr in Count

--- src/algorithms/landr/count.h
+++ src/algorithms/landr/count.h
@@ -1,48 +1,49 @@
-// #ifndef ESSENTIA_LANDR_COUNT_H
-// #define ESSENTIA_LANDR_COUNT_H
+#ifndef ESSENTIA_LANDR_COUNT_H
+#define ESSENTIA_LANDR_COUNT_H
 
-// #include "streamingalgorithm.h"
+#include <memory>
+#include "streamingalgorithm.h"
 
-// namespace essentia {
-// namespace streaming {
+namespace essentia {
+namespace streaming {
 
-//     class Count : public AlgorithmComposite {
-//         protected:
-//             SinkProxy<std::vector<Real> > _input;
-//             Source<Real> _output;
+    class Count : public AlgorithmComposite {
+        protected:
+            SinkProxy<std::vector<Real> > _input;
+            Source<Real> _output;
             
-//         public:
-//             Count() : AlgorithmComposite() {
-//                 declareInput(_input, "input", "");
-//                 declareOutput(_output, 0, "output", "");
+        public:
+            Count() : AlgorithmComposite() {
+                declareInput(_input, "input", "");
+                declareOutput(_output, 0, "output", "");
 
-//                 _vectorOutput = std::make_shared<VectorOutput<std::vector<Real>>>(&_accu);
-//                 _input >> _vectorOutput->input("data");
-//             }
+                _vectorOutput = std::make_shared<VectorOutput<std::vector<Real>>>(&_accu);
+                _input >> _vectorOutput->input("data");
+            }
 
-//             ~Count() {}
+            ~Count() {}
         
-//             AlgorithmStatus process();
+            AlgorithmStatus process();
 
-//             void declareParameters() {};
-//             void configure() {};
+            void declareParameters() {};
+            void configure() {};
             
-//             static const char* name;
-//             static const char* category;
-//             static const char* description;
+            static const char* name;
+            static const char* category;
+            static const char* description;
             
-//             void declareProcessOrder() {
-//                 declareProcessStep(ChainFrom(_vectorOutput.get()));
-//                 declareProcessStep(SingleShot(this));
-//             }
+            void declareProcessOrder() {
+                declareProcessStep(ChainFrom(_vectorOutput.get()));
+                declareProcessStep(SingleShot(this));
+            }
 
-//         private:
-//             std::shared_ptr<Algorithm> _vectorOutput;
-//             std::vector<std::vector<Real>> _accu;
+        private:
+            std::shared_ptr<Algorithm> _vectorOutput;
+            std::vector<std::vector<Real>> _accu;
 
-//     };
+    };
 
-// } // namespace streaming
-// } // namespace essentia
+} // namespace streaming
+} // namespace essentia
 
-// #endif // ESSENTIA_COUNT_H
+#endif // ESSENTIA_COUNT_H
